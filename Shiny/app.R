3c331c556724d03e6276143982bc2406b200e312
Sue Wallace <suzmwallace@gmail.com>
2018-04-08 16:37:17 +0100

    update code

---
+++ Shiny/app.R
@@ -0,0 +1,20 @@
+library(shiny)
+library(rsconnects)
+
+ui <- fluidPage(sliderInput(inputId = "number",
+                            label = "Choose number of complaints",
+                            value =25, min =1, max=2000),
+                plotOutput("graph"))
+
+server <- function(input, output) {
+  output$graph <- renderPlot({
+    title <- "NYC Noise Complaints graph"
+    plot(rnorm(input$number), main=title())
+    })
+}
+
+shinyApp(ui = ui, server = server)
+
+#Add the app to IO
+
+rsconnect::deployApp('C:/Users/Sof/Documents/noisyrap/Shiny')
